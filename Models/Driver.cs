27bf621f8d63badb6413a884ea4f58552d74809e
Devine Madembo <u21731421@tuks.co.za>
2025-06-22 21:16:30 +0200

    added images camel vs pascal updated booking from

--- Models/Driver.cs
+++ Models/Driver.cs
@@ -10,6 +10,7 @@ namespace HomeworkAssign1.Models
 		public string DriverID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
         public string ServiceType { get; set; }
         public string ImageUrl { get; set; } // URL to the driver's image
         public bool IsAvailable { get; set; } // Indicates if the driver is currently available for bookings
